469a971cd7b3bacfd5b3a8c2820d5cd384beec8d
Thennarasu24 <141826323+Thennarasu24@users.noreply.github.com>
2023-08-11 10:24:52 +0530

    Add files via upload

---
+++ day2/Mobile.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int batteryCapacity = 3000;
+		boolean touchDisplay = true;
+		char mobModel = 'I';
+		float chargingTime = 1.3f;
+		String modelName = "OnePlus";
+
+		
+		System.out.println(batteryCapacity);
+		System.out.println(touchDisplay);
+		System.out.println(mobModel);
+		System.out.println(chargingTime);
+		System.out.println(modelName);
+		
+	}
+
+}
